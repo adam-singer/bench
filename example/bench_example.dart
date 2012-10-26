a07a8915757ef8e31ba21cda9cf4c335f2a77517
rmsmith <ross@honey>
2012-10-25 20:28:28 -0700

    commented the examples

--- example/bench_example.dart
+++ example/bench_example.dart
@@ -7,13 +7,25 @@ import 'package:logging/logging.dart';
 
 void main() {
   
+  // Bench logs to a Logger named 'bench'; you may use the logging API to
+  // change the log level and add handlers to that logger or the root logger.
   Logger.root.on.record.add((record) => print('${record.message}'));
   
+  // Create and run() a Benchmarker object; this will detect all of the
+  // top-level functions in the current isolate which meet the criteria:
+  //    - the function must return a Benchmark
+  //    - the function must have no arguments
+  // You may optionally pass a number of global iterations to run for all
+  // benchmark functions in the isolate.
   new Benchmarker().run();
 }
 
+/// This is an example of a synchronous Benchmark with setup
+/// see http://en.wikipedia.org/wiki/Pollard's_rho_algorithm
 Benchmark pollardRho() {
-  // http://en.wikipedia.org/wiki/Pollard's_rho_algorithm
+  
+  // Perform your setup code in this scope; it will be executed once per
+  // iteration of the entire library.
   
   int gcd(int a, int b) {
     while(b > 0) {
@@ -39,14 +51,24 @@ Benchmark pollardRho() {
   
   int n = 329569479697;
   
-  return new Benchmark(() {
-    rho(n);
-  }, warmup:1, measure:2, description:"Pollard's rho algorithm for n: ${n}");
+  // Return a Benchmark object that encapsulates the information bench will
+  // need in order to perform your benchmark.
+  return new Benchmark(
+      () => rho(n), // The function that will be called for each iteration
+      warmup:1, // The number of warmup iterations that you want   
+      measure:2, // The number of measured iterations that you want
+      description:"Pollard's rho algorithm for n: ${n}"); // A description
 }
 
-Benchmark timer() => new Benchmark.async(Future async() {
+/// This is an example of an asynchronous Benchmark without any setup
+/// Notice that we are using the [Benchmark.async] constructor to indicate this.
+Benchmark timer() => new Benchmark.async(
+    Future async() { // The function for asynchronous benchmarks returns Future
+      
   var completer = new Completer();
   
+  // The timer will complete the Future and that will trigger bench to 
+  // advance to the next iteration of the benchmark or finish.
   new Timer(1500, (t) => completer.complete(null));
   
   return completer.future;
